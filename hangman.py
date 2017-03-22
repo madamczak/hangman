f9ac225e71e1f10d9b7769baddbac2b75d426052
madamczak <fakeemail@tro.lolo>
2017-03-22 19:07:04 +0100

    add new files

---
+++ hangman.py
@@ -0,0 +1,11 @@
+#method readInWords that takes a file path(to the file with words) as an argument and returns a list of words longer than 5 letters.
+#initialize variable listOfWords and set it to empty list
+
+#initailize variable fileWithWords and set it to the opened file
+#initialize varialbe wordsFromFile and use readlines method to set it to the list of words from fileWithWords
+
+#iterate over each word in wordsFromFile
+#initialize variable strippedWord and set it to word without whitespaces (use strip method)
+#check if length of strippedWord is greater than 5. If it is add it to listOfWords
+
+#return listOfWords at the end of readInWords method
